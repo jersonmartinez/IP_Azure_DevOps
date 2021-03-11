66296af74df629faa6d74ad752262740553e3456
Jerson Martinez <jersonmartinezsm@gmail.com>
2021-03-11 15:37:36 -0600

    Save

---
+++ getIPs.py
@@ -0,0 +1,6 @@
+import os
+os.system('cls')
+
+os.system("curl -sS https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519 | egrep -o 'https://download.*?\.json';")
+
+os.system("echo Hello from the other side!")
