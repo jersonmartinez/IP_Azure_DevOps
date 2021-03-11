e26762ee82e17854ce5245be09e01dece643aef3
Jerson Martinez <jersonmartinezsm@gmail.com>
2021-03-11 16:45:56 -0600

    Change

--- IP.py
+++ IP.py
@@ -1,11 +1,9 @@
+import urllib, json, requests
 from tqdm import tqdm
-import urllib, json
-import requests
 from bs4 import BeautifulSoup
  
 def getLink(url):
-    reqs = requests.get(url)
-    soup = BeautifulSoup(reqs.text, 'html.parser')
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
     urls = []
     
     for link in soup.find_all('a'):
@@ -16,19 +14,12 @@ def getLink(url):
 
 def downloadJSON(script_json):
     filename = script_json.split("/")[len(script_json.split("/")) - 1]
-
     response = requests.get(script_json, stream=True)
 
-    with open(filename, "wb") as handle:
+    with open("JSON/" + filename, "wb") as handle:
         for data in tqdm(response.iter_content()):
             handle.write(data)
     
-    AnalizeIPs()
-
-def AnalizeIPs():
-    
-
-
 if __name__ == "__main__":
     url = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519"
     getLink(url)
